c7116f386200a8f6bbe0fec4632aff96e65db2ad
jasontaff <jason.dion.taff@gmail.com>
2019-10-15 17:15:15 -0400

    bug with lession 11, on transition to

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {getFunName} from'../helpers';
 
 class StorePicker extends React.Component{
@@ -11,15 +12,21 @@ class StorePicker extends React.Component{
 
     goToStore(event){
         event.preventDefault();
-        console.log(this.storeInput.value);
+      
         //first grab text from input field
+        const storeId = this.storeInput.value;
+        console.log(storeId);
+    
+        this.context.router.transitionTo('/store/');
+
         //second, transisiton from '/' to '/store-id'/
+      
     }
 
 
     render(){
         return (
-            <form className="store-selector" onSubmit={this.goToStore.bind(this)} >
+            <form className="store-selector" onSubmit={(e)=> this.goToStore(e)} >
                 {/* test comment */}
                 <h2>Please Enter a Store</h2>
                 <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
@@ -29,6 +36,10 @@ class StorePicker extends React.Component{
 
         )
     }
+}
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
 }
 
 export default StorePicker;
